2ecfb8e4e5e6044fb35c44ba02c2241b3ecbffec
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2017-08-20 17:10:17 +0900

    Add app

---
+++ AppArchitectureSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppArchitectureSample
+//
+//  Created by Yoshikuni Kato on 8/20/17.
+//  Copyright © 2017 Yoshikuni Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
