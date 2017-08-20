c07f008fcc4ef224d6a596d8c23018363cb59d5a
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2017-08-20 17:31:47 +0900

    Add root window & root vc

--- AppArchitectureSample/Application/AppDelegate.swift
+++ AppArchitectureSample/Application/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootVC = UIViewController()
+        rootVC.view.backgroundColor = .orange
+        window?.rootViewController = rootVC
+
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
